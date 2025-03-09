50e930b2a968d5c48677cc053ede2079d3cf6a22
catalinfortest <catalinmucica.test@gmail.com>
2025-03-09 18:01:38 +0200

    Remove building tests with work lib

--- main.cpp
+++ main.cpp
@@ -2,14 +2,6 @@
 
 #include <cstdint>
 
-// auto func(const int32_t num) -> int32_t
-// {
-//     if (num > 0)
-//         return num;
-//     else
-//         return 0;
-// }
-
 auto main() -> int32_t
 {
     const int32_t num = 1;
