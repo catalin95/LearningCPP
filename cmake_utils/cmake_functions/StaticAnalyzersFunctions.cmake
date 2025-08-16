cc3712f6f47aaf437432e0cd407a7e3c3edb8ef0
catalinfortest <catalinmucica.test@gmail.com>
2025-08-16 17:04:44 +0300

    More modifications to cmake

--- cmake_utils/cmake_functions/StaticAnalyzersFunctions.cmake
+++ cmake_utils/cmake_functions/StaticAnalyzersFunctions.cmake
@@ -1,27 +1,46 @@
 function(enable_clang_tidy target)
-    # Make sure target exists
     if(NOT TARGET ${target})
         message(FATAL_ERROR "Target ${target} does not exist")
     endif()
 
-    # Build the compile commands for the target
     get_target_property(SOURCES ${target} SOURCES)
     if(NOT SOURCES)
         message(WARNING "Target ${target} has no sources to run clang-tidy on")
         return()
     endif()
 
-    # Optional: set clang-tidy binary (can also use env var)
     set(CLANG_TIDY_BINARY "clang-tidy")
 
-    # Add a custom target to run clang-tidy, and it will be tidy_ + binary name or target
     add_custom_target(
-    tidy_${target}
-    COMMAND ${CMAKE_COMMAND} -E echo "Running clang-tidy on target ${target}..."
-    COMMAND ${CLANG_TIDY_BINARY} -checks=cppcoreguidelines-owning-memory ${SOURCES} -p ${CMAKE_BINARY_DIR} -- -std=gnu++23 -isysroot ${CMAKE_OSX_SYSROOT}
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    COMMENT "Running clang-tidy static analysis"
-    VERBATIM
-)
+        tidy_${target}
+        COMMAND ${CMAKE_COMMAND} -E echo "Running clang-tidy on target ${target}..."
+        COMMAND ${CLANG_TIDY_BINARY} -p ${CMAKE_BINARY_DIR} ${SOURCES}
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        COMMENT "Running clang-tidy static analysis"
+        VERBATIM
+    )
+endfunction()
 
+function(enable_cppcheck target)
+    if(NOT TARGET ${target})
+        message(FATAL_ERROR "Target ${target} does not exist")
+    endif()
+
+    set(CPPCHECK_BINARY "cppcheck")
+    set(CPPCHECK_FLAGS
+        --enable=all
+        --std=c++23
+        --inconclusive
+        --quiet
+        --suppress=missingIncludeSystem
+    )
+
+    add_custom_target(
+        cppcheck_${target}
+        COMMAND ${CMAKE_COMMAND} -E echo "Running cppcheck on target ${target}..."
+        COMMAND ${CPPCHECK_BINARY} ${CPPCHECK_FLAGS} --project=${CMAKE_BINARY_DIR}/compile_commands.json
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        COMMENT "Running cppcheck static analysis"
+        VERBATIM
+    )
 endfunction()
