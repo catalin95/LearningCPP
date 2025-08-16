efc629ee0ea9ceaff5a1e1017adf2b7547fad1ea
catalinfortest <catalinmucica.test@gmail.com>
2025-08-16 17:58:04 +0300

    cleaning coverage functions in cmake

--- cmake_utils/cmake_functions/CoverageFunctions.cmake
+++ cmake_utils/cmake_functions/CoverageFunctions.cmake
@@ -1,5 +1,4 @@
 function(enable_coverage tests)
-
     if (APPLE)
         message(STATUS "Configuring coverage for Apple/Clang...")
         # Determine compiler path
@@ -46,47 +45,3 @@ function(enable_coverage tests)
         COMMENT "Generating code coverage report using LLVM tools"
     )
 endfunction()
-
-
-# function(enable_coverage tests)
-#     if(APPLE)
-#         message(STATUS "Configuring coverage for Apple/Clang...")
-
-#         # Detect compiler path
-#         get_filename_component(COMPILER_PATH ${CMAKE_CXX_COMPILER} REALPATH)
-
-#         if(COMPILER_PATH MATCHES ".*/usr/bin/clang\\+\\+$")
-#             # Apple Xcode LLVM
-#             set(LLVM_PROFDATA "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/llvm-profdata")
-#             set(LLVM_COV "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/llvm-cov")
-#         else()
-#             # Try to find in PATH
-#             find_program(LLVM_PROFDATA llvm-profdata)
-#             find_program(LLVM_COV llvm-cov)
-#         endif()
-
-#     elseif(UNIX)
-#         message(STATUS "Configuring coverage for generic Unix...")
-
-#         # Assume LLVM tools available in PATH
-#         find_program(LLVM_PROFDATA llvm-profdata)
-#         find_program(LLVM_COV llvm-cov)
-
-#         if(NOT LLVM_PROFDATA OR NOT LLVM_COV)
-#             message(FATAL_ERROR "llvm-profdata/llvm-cov not found on this system")
-#         endif()
-#     else()
-#         message(FATAL_ERROR "Coverage not supported on this platform")
-#     endif()
-
-#     message(STATUS "Using llvm-profdata: ${LLVM_PROFDATA}")
-#     message(STATUS "Using llvm-cov: ${LLVM_COV}")
-
-#     add_custom_target(coverage
-#         COMMAND ${CMAKE_COMMAND} -E env LLVM_PROFILE_FILE=${CMAKE_BINARY_DIR}/default.profraw $<TARGET_FILE:${tests}>
-#         COMMAND ${LLVM_PROFDATA} merge -sparse ${CMAKE_BINARY_DIR}/default.profraw -o ${CMAKE_BINARY_DIR}/default.profdata
-#         COMMAND ${LLVM_COV} show --format=html --instr-profile=${CMAKE_BINARY_DIR}/default.profdata -o ${CMAKE_BINARY_DIR}/coverage_report $<TARGET_FILE:${tests}> --ignore-filename-regex '/usr/*' --use-color
-#         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-#         COMMENT "Generating code coverage report using LLVM tools"
-#     )
-# endfunction()
